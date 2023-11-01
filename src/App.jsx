fa105f0b67849908816ae0a36dbe5c2d1250f313
Emelie Segell <segell.emelie@gmail.com>
2023-11-01 20:47:06 +0100

    add plan and notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
 export const App = () => {
   return <div>Find me in src/app.jsx!</div>;
 };
+
+// - tweet container component
+
+// - post tweet component
+
+// - tweet component
+// time of tweet
+// hearts
+// message
+
+// - post heart component
+
+// 1. fetch data and displaying in a container as a card
+// create and style thoughts
+// fetch the data
+// render new thoughts when they are created (useEffect)
+// calulate how old each thought is
+// 2. post new thoughts
+// get information about the time
+// get information about the timezone
+// 3. post hearts
+// create a hearts button
